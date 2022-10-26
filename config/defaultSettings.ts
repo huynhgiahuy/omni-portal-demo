f74ab67d843912f35a080b82a5ee8ee63cf2fbf7
SangTT9 <sangtt9@fpt.com.vn>
2022-10-26 15:23:20 +0700

    :tada: Initial commit

---
+++ config/defaultSettings.ts
@@ -0,0 +1,23 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-layout';
+import logoTheme from '../public/logo_theme.svg';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'side',
+  contentWidth: 'Fixed',
+  fixedHeader: true,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'OmniChannel',
+  pwa: false,
+  // logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: logoTheme,
+  iconfontUrl: '',
+};
+
+export default Settings;
