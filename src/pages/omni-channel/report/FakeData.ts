977f1add678343aa79eaa934083a96891c3cc1bc
huyhg3 <huyhg3@fpt.com.vn>
2022-11-03 16:30:50 +0700

    Add History Call Page

---
+++ src/pages/omni-channel/report/FakeData.ts
@@ -0,0 +1,161 @@
+interface DataType {
+  key: string;
+  huongcuocgoi: string;
+  somaygoi: string;
+  tennguoigoi: string;
+  somaynhan: string;
+  tennguoinhan: string;
+  thoigianbatdau: string;
+  thoiluong: string;
+  ketqua: string;
+  ghiam: string;
+  ghichu: string;
+}
+
+interface DataTypeDanhBa {
+  key: string;
+  stt: string;
+  hovaten: string;
+  sodidong: string;
+  somaynhanh: string;
+  email: string;
+  loai: string;
+}
+
+export const data: DataType[] = [
+  {
+    key: '1',
+    huongcuocgoi: 'gọi vào',
+    somaygoi: '09098029992',
+    tennguoigoi: 'Nguyễn Thu Hương',
+    somaynhan: '109',
+    tennguoinhan: 'Trần Hồng Quân',
+    thoigianbatdau: '31/01/2022 19:09:09',
+    thoiluong: '00:11:16',
+    ketqua: 'Nhỡ trong hàng chờ',
+    ghiam: 'ok',
+    ghichu: 'Hỗ trợ ticket SC01293192312312. Hỗ trợ sớm nha anh chị. Cảm ơn anh chị',
+  },
+  {
+    key: '2',
+    huongcuocgoi: 'gọi vào',
+    somaygoi: '09098029992',
+    tennguoigoi: 'Nguyễn Thu Hương',
+    somaynhan: '109',
+    tennguoinhan: 'Trần Hồng Quân',
+    thoigianbatdau: '31/01/2022 19:09:09',
+    thoiluong: '00:11:16',
+    ketqua: 'Thành công',
+    ghiam: 'ok',
+    ghichu: 'test',
+  },
+  {
+    key: '3',
+    huongcuocgoi: 'gọi ra',
+    somaygoi: '09098029992',
+    tennguoigoi: 'Nguyễn Thu Hương',
+    somaynhan: '109',
+    tennguoinhan: 'Trần Hồng Quân',
+    thoigianbatdau: '31/01/2022 19:09:09',
+    thoiluong: '00:11:16',
+    ketqua: 'Thành công',
+    ghiam: 'ok',
+    ghichu: 'test',
+  },
+  {
+    key: '4',
+    huongcuocgoi: 'gọi ra',
+    somaygoi: '09098029992',
+    tennguoigoi: 'Nguyễn Thu Hương',
+    somaynhan: '109',
+    tennguoinhan: 'Trần Hồng Quân',
+    thoigianbatdau: '31/01/2022 19:09:09',
+    thoiluong: '00:11:16',
+    ketqua: 'Thành công',
+    ghiam: 'ok',
+    ghichu: 'test',
+  },
+  {
+    key: '5',
+    huongcuocgoi: 'gọi vào',
+    somaygoi: '09098029992',
+    tennguoigoi: 'Nguyễn Thu Hương',
+    somaynhan: '109',
+    tennguoinhan: 'Trần Hồng Quân',
+    thoigianbatdau: '31/01/2022 19:09:09',
+    thoiluong: '00:11:16',
+    ketqua: 'Thành công',
+    ghiam: 'ok',
+    ghichu: 'test',
+  },
+  {
+    key: '6',
+    huongcuocgoi: 'gọi ra',
+    somaygoi: '09098029992',
+    tennguoigoi: 'Nguyễn Thu Hương',
+    somaynhan: '109',
+    tennguoinhan: 'Trần Hồng Quân',
+    thoigianbatdau: '31/01/2022 19:09:09',
+    thoiluong: '00:11:16',
+    ketqua: 'Thành công',
+    ghiam: 'ok',
+    ghichu: 'test',
+  },
+];
+
+export const dataDanhba: DataTypeDanhBa[] = [
+  {
+    key: '1',
+    stt: '',
+    hovaten: 'Robert Fox',
+    sodidong: '0909090909',
+    somaynhanh: '449003',
+    email: 'robertfox@gmail.com',
+    loai: 'Nội bộ',
+  },
+  {
+    key: '2',
+    stt: '',
+    hovaten: 'Robert Fox',
+    sodidong: '0909090909',
+    somaynhanh: '449003',
+    email: 'robertfox@gmail.com',
+    loai: 'Nội bộ',
+  },
+  {
+    key: '3',
+    stt: '',
+    hovaten: 'Robert Fox',
+    sodidong: '0909090909',
+    somaynhanh: '449003',
+    email: 'robertfox@gmail.com',
+    loai: 'Nội bộ',
+  },
+  {
+    key: '4',
+    stt: '',
+    hovaten: 'Robert Fox',
+    sodidong: '0909090909',
+    somaynhanh: '449003',
+    email: 'robertfox@gmail.com',
+    loai: 'Nội bộ',
+  },
+  {
+    key: '5',
+    stt: '',
+    hovaten: 'Robert Fox',
+    sodidong: '0909090909',
+    somaynhanh: '449003',
+    email: 'robertfox@gmail.com',
+    loai: 'Nội bộ',
+  },
+  {
+    key: '6',
+    stt: '',
+    hovaten: 'Robert Fox',
+    sodidong: '0909090909',
+    somaynhanh: '449003',
+    email: 'robertfox@gmail.com',
+    loai: 'Nội bộ',
+  },
+];
