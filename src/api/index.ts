4b2973594e7f59a516e377129e2b38710525fe78
HUỲNH QUỐC HUY <89074615+suhuynhpr@users.noreply.github.com>
2022-11-03 15:46:27 +0700

    create env

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+export default {
+  UMI_API_BASE_URL: process.env.UMI_API_BASE_URL,
+  WS_CALL_CENTER_SERVICE: process.env.UMI_WS_CALL_CENTER_URL,
+  UMI_API_URL: process.env.UMI_API_URL,
+  PORT: process.env.PORT,
+  UMI_DOMAIN: process.env.UMI_DOMAIN,
+  REDIRECT_URI_DOMAIN: process.env.REDIRECT_URI_DOMAIN,
+  ENVIROMENT: process.env.ENVIROMENT,
+  REDIRECT_URI_PROTOCOL: process.env.REDIRECT_URI_PROTOCOL,
+  TENANT_NAME: process.env.TENANT_NAME,
+  CLIENT_ID: process.env.CLIENT_ID,
+  SECRET_ID: process.env.SECRET_ID,
+  ENV: process.env.ENV,
+};
