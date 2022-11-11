3d6135e0d4582e0afd9ef5eff90245b7d428a56e
Huỳnh Quốc Huy <huyhq29@fpt.com.vn>
2022-11-11 08:19:10 +0700

    redirect to logout

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -31,6 +31,8 @@ const loginOut = async () => {
   const { redirect } = query;
   window.localStorage.removeItem('access_token');
   window.localStorage.removeItem('rid');
+  history.push('/user/login');
+
   // Note: There may be security issues, please note
   if (window.location.pathname !== '/user/login' && !redirect) {
     history.replace({
