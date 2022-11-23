04cf48b4dac81ee6aa64c9ed058f24366ec97844
thanhnv97@fpt.com.vn <thanhnv97@fpt.com.vn>
2022-11-23 17:32:48 +0700

    build socket to call

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -10,7 +10,7 @@ import NoticeIconView from '../NoticeIcon';
 import WorkingStatus from './WorkingStatus';
 import AgentModalRing from '../AgentModalRing';
 import AgentModalAnswer from '../AgentModalAnswer';
-import io from 'socket.io-client';
+import { allStatusCall } from '../../socket';
 
 export type SiderTheme = 'light' | 'dark';
 
@@ -26,23 +26,7 @@ const GlobalHeaderRight: React.FC = () => {
   const [isVisibleNoteCall, setVisibleNoteCall] = useState(false);
   const [isActiveIconNote, setActiveIconNote] = useState(false);
 
-  const access_token = localStorage.getItem('access_token');
-  const socket = io('http://172.27.228.201:9003', {
-    path: '/ws/socket.io',
-    auth: {
-      token: access_token,
-    },
-  });
-  console.log({ socket });
-  const sendMessage = () => {
-    console.log({ message: 'send message for server' });
-    socket.emit('send_message', { message: 'Hello, I am client' });
-  };
-
-  const socket = io('http://localhost:3001');
-  console.log({ socket });
-  let { id } = socket;
-  console.log({ id });
+  console.log({ call: allStatusCall.ringingCall });
 
   if (!initialState || !initialState.settings) {
     return null;
