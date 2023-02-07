5026cc5fa68354dba549899541a419d9b1fc75bc
Huỳnh Quốc Huy <huyhq29@fpt.com.vn>
2023-02-07 15:30:32 +0700

    hide permission role

--- src/pages/omni-channel/setting/FakeData.ts
+++ src/pages/omni-channel/setting/FakeData.ts
@@ -88,34 +88,34 @@ export const dataPermission: DataType[] = [
 ];
 
 export const dataPermissionTable: DataTypePermissionTable[] = [
-  {
-    key: '1',
-    module: 'Dashboard',
-  },
-  {
-    key: '2',
-    module: 'Incident Management',
-  },
-  {
-    key: '3',
-    module: 'Event Management',
-  },
-  {
-    key: '4',
-    module: 'Change Management',
-  },
-  {
-    key: '5',
-    module: 'Request Fulfillment',
-  },
+  // {
+  //   key: '1',
+  //   module: 'Dashboard',
+  // },
+  // {
+  //   key: '2',
+  //   module: 'Incident Management',
+  // },
+  // {
+  //   key: '3',
+  //   module: 'Event Management',
+  // },
+  // {
+  //   key: '4',
+  //   module: 'Change Management',
+  // },
+  // {
+  //   key: '5',
+  //   module: 'Request Fulfillment',
+  // },
   // {
   //   key: '6',
   //   module: 'Thiết kế',
   // },
-  {
-    key: '7',
-    module: 'Report',
-  },
+  // {
+  //   key: '7',
+  //   module: 'Report',
+  // },
   {
     key: '8',
     module: 'Profile',
